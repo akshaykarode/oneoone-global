4c8b31a50455c8bd622b237d6e756aef2341ccfc
Akshay <akshay.karode@instarem.com>
2020-08-21 10:29:27 +0530

    Updates

---
+++ precache-manifest.129fb4c2fa112277355f1a8396fb3c5d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "644ba867709c5699ace703ba3a2112e2",
+    "url": "/oneoone-global/index.html"
+  },
+  {
+    "revision": "fbc2faf4ecc6d73c2ddd",
+    "url": "/oneoone-global/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "683bdc326122f2989e81",
+    "url": "/oneoone-global/static/js/2.a6b88011.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/oneoone-global/static/js/2.a6b88011.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbc2faf4ecc6d73c2ddd",
+    "url": "/oneoone-global/static/js/main.601e5275.chunk.js"
+  },
+  {
+    "revision": "2e121e3d8784b05f88dc",
+    "url": "/oneoone-global/static/js/runtime-main.c08ec6db.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/oneoone-global/static/media/logo.5d5d9eef.svg"
+  }
+]);
