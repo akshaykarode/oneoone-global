4fcffecdbc92f64d9955f67df84bfa0362cd4c8f
Akshay <akshay.karode@instarem.com>
2020-08-21 16:26:41 +0530

    Updates

---
+++ precache-manifest.83e641c9c63eddda1606b775802062a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a63c7e72c6b85497ab61c71cfc311ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d42bada8646ac7fe0e0f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "683bdc326122f2989e81",
+    "url": "/static/js/2.a6b88011.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a6b88011.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d42bada8646ac7fe0e0f",
+    "url": "/static/js/main.b72bcf0a.chunk.js"
+  },
+  {
+    "revision": "13f61419b43487eeb4d7",
+    "url": "/static/js/runtime-main.01efe64d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/oneoone-global/precache-manifest.129fb4c2fa112277355f1a8396fb3c5d.js"
+  "/precache-manifest.83e641c9c63eddda1606b775802062a4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/oneoone-global/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
